b242dd39f5dd64f5ca4d30e9ab6716c36acd5223
GelAssEnHeiT435 <108595070+GelAssEnHeiT435@users.noreply.github.com>
2025-10-29 15:26:11 +0300

    some changes

--- client/src/components/common/LoadingSpinner.jsx
+++ client/src/components/common/LoadingSpinner.jsx
@@ -6,9 +6,9 @@ function LoadingSpinner()
         <div className="loading-spinner-container">
             <div className="telegram-loading">
                 <div className="loading-spinner">
-                    <p>Загрузка Telegram Mini App...</p>
                 </div>
             </div>
+            <p>Загрузка Telegram Mini App...</p>
         </div>
     )
 }
