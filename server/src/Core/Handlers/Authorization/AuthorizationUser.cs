9f76af678a501f2c0ac3a754a9d8d45ea52494c0
GelAssEnHeiT435 <108595070+GelAssEnHeiT435@users.noreply.github.com>
2025-12-28 18:26:12 +0300

    Update AuthorizationUser.cs

--- server/src/Core/Handlers/Authorization/AuthorizationUser.cs
+++ server/src/Core/Handlers/Authorization/AuthorizationUser.cs
@@ -14,11 +14,13 @@ namespace FlowerBot.src.Core.Handlers.Authorization
 
     public class AuthorizationUserHandler : IRequestHandler<AuthorizationUserCommand, TokensResult?>
     {
+        private readonly ILogger<AuthorizationUserHandler> _logger;
         private readonly ApplicationContext _context;
         private readonly IAuthService _authService;
         private readonly string _botToken;
 
         public AuthorizationUserHandler(
+            ILogger<AuthorizationUserHandler> logger,
             ApplicationContext context, 
             IAuthService authService,
             IConfiguration configuration)
@@ -27,20 +29,32 @@ namespace FlowerBot.src.Core.Handlers.Authorization
             _authService = authService;
             _botToken = configuration["TelegramBot:Token"]
                     ?? throw new InvalidOperationException("Telegram bot token missing");
+            _logger = logger;
         }
 
         public async Task<TokensResult?> Handle(AuthorizationUserCommand request, CancellationToken cancellationToken)
         {
             if (request.InitDataRaw.IsNullOrEmpty())
+            {
+                _logger.LogWarning("InitDataRaw пустой или null");
                 return null;
+            }
 
             if (!InitDataValidator.Validate(request.InitDataRaw!, _botToken))
+            {
+                _logger.LogWarning("Валидация InitDataRaw не пройдена. Данные: {Data}", request.InitDataRaw);
                 return null; 
+            }
+                
 
             long? telegramId = InitDataValidator.GetUserId(request.InitDataRaw!);
             if (!telegramId.HasValue || telegramId <= 0)
+            {
+                _logger.LogWarning("Не удалось извлечь telegramId из InitDataRaw: {Data}", request.InitDataRaw);
                 return null;
+            }
 
+            _logger.LogInformation("Успешная валидация. Telegram ID: {TelegramId}", telegramId.Value);
             string accessToken = _authService.GenerateJwt(telegramId.Value, TimeSpan.FromMinutes(15));
 
             RefreshToken refreshToken = new RefreshToken
@@ -53,6 +67,7 @@ namespace FlowerBot.src.Core.Handlers.Authorization
             _context.RefreshTokens.Add(refreshToken);
             await _context.SaveChangesAsync(cancellationToken);
 
+            _logger.LogInformation("Токены успешно созданы для Telegram ID: {TelegramId}", telegramId.Value);
             return new TokensResult(refreshToken.Id, accessToken);
         }
     }
